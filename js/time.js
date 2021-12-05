09ddb76fe3bdd6ad273bf213da05b2af1f77ebf7
eivanovJr <Ivanov.2k.ivanov@gmail.com>
2021-12-06 00:00:40 +0300

    Add: create file time.js to lab4

---
+++ js/time.js
@@ -0,0 +1,15 @@
+var time = new Date();
+
+window.onload = function () {
+    let curTime = new Date();
+    let timer = document.getElementById("timer");
+    timer.innerText = (curTime - time).toString() + "ms";
+
+    let navElements = document.getElementsByClassName("nav-element");
+    for (let i = 0; i < navElements.length; i++) {
+        if (navElements[i].href === window.location.href)
+            navElements[i].classList.add("selected");
+    }
+
+}
+
